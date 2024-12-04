069190914040898b76e76a04cf8a381a48136bd8
demimich0ud1 <demimichoudi@gmail.com>
2024-12-04 13:49:55 +0200

    Add files via upload

---
+++ DoubleQueueImpl.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+public class StringStackImpl implements StringStackImpl
+	private static class Node{
+		double data;
+		Node next;
+			
+		Node(Double data){
+			this.data = data;
+			this.next = null;
+			}
+	}
+	
---
+++ StringStackImpl.java
@@ -0,0 +1,13 @@
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+public class StringStackImpl implements StringStackImpl
+	private static class Node{
+		String data;
+		Node next;
+			
+		Node(String data){
+			this.data = data;
+			this.next = null;
+			}
+	}
