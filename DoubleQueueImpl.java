a761f706f59f037e829d922e128282921d4f171d
demi_michoudi <demimichoudi@gmail.com>
2024-12-05 00:43:20 +0200

    Update DoubleQueueImpl.java
    added two new methods

--- DoubleQueueImpl.java
+++ DoubleQueueImpl.java
@@ -11,4 +11,44 @@ public class StringStackImpl implements StringStackImpl{
 			this.next = null;
 			}
 	}
+	private Node peak;
+	private int size;
+
+	public StringStackImpl(){
+		peak = null;
+		size = 0;
+	}
+
+	@Override
+	public boolean isEmpty(){
+		return peak == null;
+	}
+
+	public void push(String item){
+		Node newNode = new Node(item);
+		newNode.next = top;
+		peak = newNode;
+		size++;
+	}
+
+	@Override
+	public String pop(){
+		if (isEmpty()){
+			throw new NoSuchElementException("Stack is Empty"); 
+		}
+		String item = peak.data;
+		peak = peak.next;
+		size--;
+		return item;
+	}
 }
+
+
+
+
+
+
+
+
+
+
